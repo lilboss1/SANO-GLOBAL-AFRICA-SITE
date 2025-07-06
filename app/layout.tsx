db814532eb7bd5e2f92aa43b8b7393dbf513b71e
MUSIC HUSTLE <lilbossalaprod@gmail.com>
2025-07-06 20:37:38 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Sano Global',
+  description: 'Created By Sanou Ousseni',
   generator: 'v0.dev',
 }
 
